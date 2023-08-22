628210de9c01652622a5c9b5f15a005e43e898d0
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-22 18:33:55 +0100

    fixing

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 function connectDB() {
-    return mongoose.connect(process.env.MONGO_URI)
+    return mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
 }
 
 module.exports = connectDB;
